e4c38972bdfd7bb7009b2c08b4d6efa854a86269
Arturo Aguilar <artrune@hotmail.com>
2021-12-02 10:51:44 -0600

    adding rm to start file for easy rebuilding and re deploying

--- start_all.bat
+++ start_all.bat
@@ -1,5 +1,6 @@
+docker rm -f solr
+docker rm -f synonym-updater
+
 docker build --tag synonym-updater:1 .
 docker run -d -p 8983:8983 --name solr -v "%cd%\solrdata:/var/solr" -v "%cd%\conf:/mycore_config/conf" solr:8 solr-precreate mycore /mycore_config
 docker run -v "%cd%\solrdata\data\mycore\conf:/mycore_config/conf" --name synonym-updater -p 8092:8092 -d synonym-updater:1
-
-pause
